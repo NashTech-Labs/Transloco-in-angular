98b7c1a7850f675401fe5d47f1edf89db872c438
kiran-0407 <kiran.kaur@nashtechglobal.com>
2024-10-23 23:03:18 +0530

    added transloco

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,31 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TranslocoDirective, TranslocoService } from '@jsverse/transloco';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TranslocoDirective],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'translocoDemo';
+  currentLang: string = "";
+
+  constructor(private translocoService: TranslocoService) {
+    translocoService.langChanges$.subscribe(language => {
+      this.currentLang = language;
+      console.log('Current Language: ', this.currentLang);
+    });
+  }
+
+  ngOnInit(): void {
+    this.currentLang = this.translocoService.getActiveLang();
+  }
+
+  onChange(event: any) {
+    const language = event.target.value;
+    this.translocoService.setActiveLang(language);
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,21 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideTransloco } from '@jsverse/transloco';
+import { TranslocoHttpLoader } from './transloco-loader';
+import { provideHttpClient } from '@angular/common/http';
+
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), provideHttpClient(),
+    provideTransloco({
+    config: {
+      availableLangs: ['en', 'es'],
+      defaultLang: 'en',
+      reRenderOnLangChange: true,
+      prodMode: !isDevMode(),
+    },
+    loader: TranslocoHttpLoader,
+  }),]
 };
